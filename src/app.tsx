ae39fa776a209fbbcd6d43266f69a4d8858f2da5
Matt Crowder <matt.crowder@veeva.com>
2024-08-22 08:44:27 -0500

    Add counter

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react"
 
 function App() {
+    const [count, setCount] = useState(0)
+    const [countPlusOne, setCountPlusOne] = useState(0)
 
-  return (
-    <>
-        Hello world
-    </>
-  )
+    useEffect(() => {
+        setCountPlusOne(count + 1)
+    }, [count])
+    useEffect(() => {
+        if (count === 5) {
+            console.log('count is ', count)
+        }
+    }, [count])
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+            <p>{String(count)}</p>
+            <p>{String(countPlusOne)}</p>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+        </div>
+    )
 }
 
 export default App
